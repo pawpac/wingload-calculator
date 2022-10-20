ac343923afc505e943f4779f83960d6f71ded473
Pawel Paczkowski <paw.pac@yahoo.com>
2022-10-20 00:09:38 -0500

    Rig control sliders with width of canopy and jumper

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ const sizeSlider = document.querySelector('.size-slider');
 const jumperDigit = document.querySelector('.jumper-digits');
 const weightSlider = document.querySelector('.weight-slider');
 
+const canopy = document.querySelector('.canopy');
+const jumper = document.querySelector('.jumper');
+const lines = document.querySelector('.lines');
+
 canopyDigit.textContent = sizeSlider.value;
 
 sizeSlider.oninput = function(){
   canopyDigit.textContent = sizeSlider.value;
   wingloading.textContent = 'Wingloading: ' + (sizeSlider.value / weightSlider.value).toFixed(2);
+
+  canopy.style.width = 70 + ((sizeSlider.value -70) / 3) + '%';
 }
 
 jumperDigit.textContent = weightSlider.value;
@@ -18,6 +24,9 @@ jumperDigit.textContent = weightSlider.value;
 weightSlider.oninput = function(){
   jumperDigit.textContent = weightSlider.value;
   wingloading.textContent = 'Wingloading: ' + (sizeSlider.value / weightSlider.value).toFixed(2);
+
+  jumper.style.width = 70 + ((weightSlider.value -70) / 3) + '%';
+  lines.style.width = 70 + ((weightSlider.value -70) / 3) + '%';
 }
 
 
